2be69b8767f622928479951d636df27678ee493d
Raj <121531101+Saheel-Raj@users.noreply.github.com>
2023-08-11 15:27:25 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,6 +8,17 @@ function counter(){
 	    $('.preloader').addClass('active')
 	    }
 	},18)
+
+  var animData = {
+		wrapper: document.querySelector('#animationWindow'),
+		animType: 'svg',
+		loop: false,
+		prerender: true,
+		autoplay: true,
+		path: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/35984/LEGO_loader.json'
+	};
+	var anim = bodymovin.loadAnimation(animData);
+  anim.setSpeed(9);
 }
 counter()
 
